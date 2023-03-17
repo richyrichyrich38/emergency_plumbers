c76caedf0068d95f447ae0eac1111b7709980477
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2023-03-17 14:43:42 +0000

    javascript form functionality  added

---
+++ app.js
@@ -0,0 +1,37 @@
+// get the form element and listen for a submit event
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // prevent the default form submission behavior
+  
+  // get the form inputs
+  const name = form.querySelector('input[name="name"]').value;
+  const email = form.querySelector('input[name="email"]').value;
+  const message = form.querySelector('textarea[name="message"]').value;
+  
+  // validate the inputs
+  if (name === '' || email === '' || message === '') {
+    alert('Please fill out all fields');
+    return;
+  }
+  
+  // send the form data to a server using fetch or another method
+  // for example:
+  fetch('/api/contact', {
+    method: 'POST',
+    body: JSON.stringify({ name, email, message }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.success) {
+      alert('Thank you for contacting us!');
+      form.reset();
+    } else {
+      alert('An error occurred. Please try again later.');
+    }
+  })
+  .catch(error => {
+    console.error(error);
+    alert('An error occurred. Please try again later.');
+  });
+});
